976aa587c7467d042f9a5f2a6b6b9d32cf9ce74e
kidow <wcgo2ling@hanmail.net>
2019-08-31 01:49:20 +0900

    mistake

--- app/pages/signup/social/index.vue
+++ app/pages/signup/social/index.vue
@@ -132,7 +132,6 @@ export default {
     }
     try {
       const { data } = await app.$axios(options)
-      console.log(data)
       if (!data) return redirect('/')
       return {
         email: data.email,
